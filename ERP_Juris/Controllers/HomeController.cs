51e437dee9269a2aba24ec2c13aaa9a5bb3c2de3
Roberto Telles <robertotelles00@gmail.com>
2021-02-22 19:10:45 -0300

    Upload 22/02/2021 7

--- ERP_Juris/Controllers/HomeController.cs
+++ ERP_Juris/Controllers/HomeController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ApplicationServices.Interfaces;
+using EntitiesServices.Model;
+using System.Globalization;
+using ERP_Juris.App_Start;
+using EntitiesServices.Work_Classes;
+using AutoMapper;
+using ERP_Juris.ViewModels;
+using System.IO;
 
 namespace ERP_Juris.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUsuarioAppService baseApp;
+        private readonly INotificacaoAppService notiApp;
+        private readonly ILogAppService logApp;
+        private readonly ITarefaAppService tarApp;
+        private String msg;
+        private Exception exception;
+        USUARIO objeto = new USUARIO();
+        USUARIO objetoAntes = new USUARIO();
+        List<USUARIO> listaMaster = new List<USUARIO>();
+
+        public HomeController(IUsuarioAppService baseApps, ILogAppService logApps, INotificacaoAppService notApps, ITarefaAppService tarApps)
+        {
+            baseApp = baseApps;
+            logApp = logApps;
+            notiApp = notApps;
+            tarApp = tarApps;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Login", "ControleAcesso");
         }
 
         public ActionResult About()
